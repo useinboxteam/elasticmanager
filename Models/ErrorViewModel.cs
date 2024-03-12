5f5477dda6d7188351a4541d08ad2334d235ba57
sercanalkan <sercanalkan@gmail.com>
2024-03-12 16:29:40 +0300

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ElasticManager.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
